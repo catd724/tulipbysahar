17c27f43b9b299811e10f7e97dbc93317639e8b2
catd724 <157598049+catd724@users.noreply.github.com>
2024-06-16 11:50:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from dotenv import load_dotenv
 
 # Load environment variables
 # Configure the API key using Streamlit secrets
-googlekey = st.secrets["auth_token"]
-genai.configure(api_key=googlekey)
+GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=GOOGLE_API_KEY)
 
 # Initialize chat history and current QA state
 if "chat_history" not in st.session_state:
